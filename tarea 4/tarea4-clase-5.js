15fd686ba311d2c0259476b1a9f6ec27e4b3e7c1
MRAS23 <101474625+MRAS23@users.noreply.github.com>
2022-06-16 21:51:32 -0300

    tarea 4 - clase 5
    subo los archivos correspondientes a la tarea 4 de la clase 5

---
+++ tarea 4/tarea4-clase-5.js
@@ -0,0 +1,74 @@
+//TAREA: En otro archivo distinto,
+// Crear una lista de <ol> y <li> que contengan sólo números.
+// Convertir esos números a un array y:
+// 1. calcular el promedio y mostrarlo en un <em> pre-creado con el texto "El promedio es..."
+// 2. obtener el número más pequeño y mostrarlo en un <em> pre-creado con el texto "El número más pequeño es..."
+// 3. obtener el número más grande y mostrarlo en un <em> pre-creado con el texto "El número más grande es..."
+// 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
+
+function calculaPromedioNumeros(numeros) {
+  let promedio = 0;
+
+  for (let i = 0; i < numeros.length; i++) {
+    promedio += numeros[i];
+  }
+
+  return promedio / numeros.length;
+}
+
+function calculaMayorNumero(numeros) {
+  let mayor = numeros[0];
+  for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] > mayor) {
+      mayor = numeros[i];
+    }
+  }
+  return mayor;
+}
+
+function calculaMenorNumero(numeros) {
+  let menor = numeros[0];
+  for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] < menor) {
+      menor = numeros[i];
+    }
+  }
+  return menor;
+}
+
+function calcularNumeroMasRepetido(numeros) {
+  let contador = 0;
+  let contadorMasRepetido = 0;
+  let repetido;
+  let masRepetido;
+  for (let i = 0; i < numeros.length; i++) {
+    repetido = numeros[i];
+    for (let j = 0; j < numeros.length; j++) {
+      if (repetido === numeros[j]) {
+        contador++;
+      }
+    }
+    if (contador > contadorMasRepetido) {
+      contadorMasRepetido = contador;
+      masRepetido = repetido;
+    }
+    contador = 0;
+  }
+  return masRepetido;
+}
+
+let $NumerosNodeLis = document.querySelectorAll("li");
+
+document.querySelector("#calcula-promedio").onclick = function () {
+  let listaNumeros = [];
+  for (let i = 0; i < $NumerosNodeLis.length; i++) {
+    listaNumeros[i] = Number($NumerosNodeLis[i].innerText);
+  }
+
+  document.querySelector("#promedio").innerText = `${calculaPromedioNumeros(listaNumeros)}`;
+  document.querySelector("#mayor").innerText = `${calculaMayorNumero(listaNumeros)}`;
+  document.querySelector("#menor").innerText = `${calculaMenorNumero(listaNumeros)}`;
+  document.querySelector("#frecuente").innerText = `${calcularNumeroMasRepetido(listaNumeros)}`;
+
+  return false;
+};
